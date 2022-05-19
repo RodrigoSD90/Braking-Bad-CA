15d57e2e2881db9d78520f2a78447f2305e4b00e
RodrigoSD90 <rodrigo.sdnz@gmail.com>
2022-05-19 22:17:02 +0000

    commit3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)}
+  { path: '', loadChildren: () => import('./pages/tabs/tabs.module').then( m => m.TabsPageModule)},
+  {
+    path: 'deaths-details',
+    loadChildren: () => import('./pages/deaths-details/deaths-details.module').then( m => m.DeathsDetailsPageModule)
+  }
 ];
 
 @NgModule({
